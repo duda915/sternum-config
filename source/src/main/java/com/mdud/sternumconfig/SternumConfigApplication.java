524f89a25d3db2003be92d19fc465d5b97aee2af
MDud <duda915@gmail.com>
2019-04-03 19:08:38 +0200

    spring init

---
+++ source/src/main/java/com/mdud/sternumconfig/SternumConfigApplication.java
@@ -0,0 +1,13 @@
+package com.mdud.sternumconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SternumConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SternumConfigApplication.class, args);
+	}
+
+}
---
+++ source/src/test/java/com/mdud/sternumconfig/SternumConfigApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mdud.sternumconfig;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SternumConfigApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
